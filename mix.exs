41cf0b6bdcb0589b26b396716a02e6ad7830033c
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-07-04 20:19:46 +0200

    release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Anoma.MixProject do
   def project do
     [
       app: :anoma,
-      version: "0.1.9",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
